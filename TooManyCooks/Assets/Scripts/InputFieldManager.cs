cff9b5317905eaf527011519c90847b3a920b096
Lukas Margaux <m.lukas@rubika-edu.com>
2020-03-28 21:16:08 +0100

    All Menus Done

---
+++ TooManyCooks/Assets/Scripts/InputFieldManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent (typeof (Selectable))]
+public class InputFieldManager : InputField, IPointerClickHandler
+{
+    public InputField field;
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        field = this;
+        field.GetComponent<RectTransform>().localPosition = new Vector3(0, 262, 0);
+        field.transform.parent.GetChild(0).gameObject.SetActive(false);
+    }
+}
